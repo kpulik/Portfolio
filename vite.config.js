0463f8e9957cc70c2c65903b7f12a77413e92048
k_pulik <63879539+kpulik@users.noreply.github.com>
2025-03-15 11:38:01 -0700

    added comments to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', 
+  base: '/Portfolio/', // 👈 critical for GitHub pages
 })
